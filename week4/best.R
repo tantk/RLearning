8636fa8eb0aa714329598345c715323e900d71d7
tan tian kian <e0015367@u.nus.edu>
2016-10-23 21:35:49 +0800

    middle2

--- week4/best.R
+++ week4/best.R
@@ -5,12 +5,24 @@ best<-function(stateName=character(),outcomeName=character())
   ## check that state and outcome are valid
   validState<-unique(outcome[,7])
   validOutcome<-c("heart attack","heart failure","pneumonia")
-  
   if(!stateName %in% validState)
     stop("invalid state")
   if(!outcomeName %in% validOutcome)
     stop("invalid outcome")
   
-  ##return hospital name in that state with the lowest 30-day death rate
+  ##filter only the needed data
+  outcome<-outcome[c(2, 7, 11, 17, 23)]
+  names(outcome)[1] <- "name"
+  names(outcome)[2] <- "state"
+  names(outcome)[3] <- "heart attack"
+  names(outcome)[4] <- "heart failure"
+  names(outcome)[5] <- "pneumonia"
   
+  ##return hospital name in that state with the lowest 30-day death rate
+
+  outcome <-(outcome[outcome$state==stateName& outcome[outcomeName] != 'Not Available',])
+  filterByOutcome <- outcome[, outcomeName]
+  rowNum <- which.min(filterByOutcome)
+  outcome[rowNum, ]$name
 }
+
